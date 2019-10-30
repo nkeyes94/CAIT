0989fddb6bcb397c8947fc6cafe5a96c5253031a
Bryan Sadler <bryan.sadler@jefferson.edu>
2019-10-30 01:09:38 -0400

    updating client and fixing proxy errors for backend

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -12,14 +12,14 @@ class Dashboard extends Component {
     return (
       <div>
         <Helmet>
-          <script src="/js/voiceRecognition.js"></script>
-          <script defer language="javascript" src="js/creative_coding.js"></script>
-          <script defer language="javascript" src="js/canvas.js"></script>
-          <script defer language="javascript" src="js/mic.js"></script>
-          <script defer language="javascript" src="js/testLay.js"></script>
-          <script src="https://kit.fontawesome.com/e297286cae.js" crossorigin="anonymous"></script>
-          <script language="javascript" crossorigin src="js/draw.js"></script>
-          <script Access-Control-Allow-Origin="*" src="https://code.responsivevoice.org/responsivevoice.js?key=A2zNMwFx"></script>
+          <script src="/js/voiceRecognition.js"/>
+          <script defer language="javascript" src="js/creative_coding.js"/>
+          <script defer language="javascript" src="js/canvas.js"/>
+          <script defer language="javascript" src="js/mic.js"/>
+          <script defer language="javascript" src="js/testLay.js"/>
+          <script src="https://kit.fontawesome.com/e297286cae.js" crossorigin="anonymous"/>
+          <script language="javascript" crossorigin src="js/draw.js"/>
+          <script Access-Control-Allow-Origin="*" src="https://code.responsivevoice.org/responsivevoice.js?key=A2zNMwFx"/>
         </Helmet>
         <Container fluid>
           <Jumbotron />
@@ -28,7 +28,7 @@ class Dashboard extends Component {
           <Row>
             <div className="mx-auto p-4">
               <Col size="md-12">
-                <button id="thisButton" className="btn btn-success" to="/"><i id="micIcon" className="fas fa-microphone-alt fa-4x"></i></button>
+                <button id="thisButton" className="btn btn-success" to="/"><i id="micIcon" className="fas fa-microphone-alt fa-4x"/></button>
               </Col>
             </div>
           </Row>
